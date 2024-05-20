59d939dd45f78a0dd823ead347b4d6453de9f740
conner eberhardt <ceberhardt1005@gmail.com>
2024-05-20 08:45:13 -0400

    pull

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,9 +1,32 @@
 package com.techelevator;
-
+import java.util.Scanner;
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner seriesDecimals = new Scanner(System.in);
+		System.out.println("Please enter in a series of decimal values (seperated by spaces");
+		int startingNum = seriesDecimals.nextInt();
 
+
+		String binaryEnd = "";
+
+		while(startingNum >= 1){
+			if(startingNum % 2 == 0){
+				startingNum = (startingNum / 2);
+				binaryEnd = 0 + binaryEnd;
+			}
+
+			else if (startingNum % 2 == 1) {
+				startingNum = (startingNum / 2);
+				binaryEnd = 1 + binaryEnd;
+			}
+
+			else if (startingNum < 1) {
+				break;
+			}
+		}
+		System.out.print(startingNum + "in binary is " + binaryEnd);
+		System.out.print(binaryEnd);
 	}
 
 }
--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -11,18 +11,14 @@ public class Fibonacci {
 
 		String zero = "0";
 // ------------------------------------------------------------ This section is good!-----------------------------------
-
 		int numOne = 0;
 		int numTwo = 1;
 		int result = 1 ;
 
-
 // -------------------------------------------------------------Defining the numbers and the sum -----------------------
 //		String starterNum = ("0, ");
 // -------------------------------------------------------------Storing the starting numbers into a value---------------
-
-
-		while(result < maxNum){
+		while(result <= maxNum){
 			result = numOne + numTwo;
 			numOne = numTwo;
 			numTwo = result;
