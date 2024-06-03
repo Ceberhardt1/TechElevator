a4a0709da065382d6578828a44c98b53fd1ccd98
conner eberhardt <ceberhardt1005@gmail.com>
2024-06-03 13:38:18 -0400

    msg 6/3/24

--- module-1/14_Unit_Testing/exercise/src/test/java/com/techelevator/CigarPartyTest.java
+++ module-1/14_Unit_Testing/exercise/src/test/java/com/techelevator/CigarPartyTest.java
@@ -1,6 +1,8 @@
 package com.techelevator;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CigarPartyTest {
 
@@ -11,11 +13,50 @@ public class CigarPartyTest {
      * successful, or false otherwise.
      */
 
-    private CigarParty sut;
+    private CigarParty cigars;
 
+    @Before
     public void setUp(){
-        sut = new CigarParty();
+        cigars = new CigarParty();
     }
+
+    @Test
+    public void cigarPartyAbove60AndBelow40OnWeekdayShouldReturnFalse(){
+
+       Assert.assertFalse(cigars.haveParty(30, false));
+       Assert.assertFalse( cigars.haveParty(65, false));
+
+    }
+
+    @Test
+    public void cigarPartyMoreThan40CigarsOnWeekendShouldReturnTrue(){
+        Assert.assertTrue(cigars.haveParty(42, true));
+        Assert.assertTrue( cigars.haveParty(90, true));
+    }
+
+    @Test
+    public void cigarPartyLessThan40OnWeekendShouldReturnFalse(){
+        Assert.assertFalse(cigars.haveParty(30, true));
+        Assert.assertFalse( cigars.haveParty(12, true));
+    }
+
+    @Test
+    public void cigarPartyBoundaryTest(){
+        Assert.assertTrue(cigars.haveParty(40, false));
+        Assert.assertTrue( cigars.haveParty(60, false));
+        Assert.assertTrue(cigars.haveParty(40, true));
+        Assert.assertFalse(cigars.haveParty(39, false));
+        Assert.assertFalse( cigars.haveParty(61, false));
+    }
+
+    @Test
+    public void cigarPartyExceptionCase(){
+        Assert.assertFalse(cigars.haveParty(-10, true));
+
+    }
+
+
+
 
 
 
