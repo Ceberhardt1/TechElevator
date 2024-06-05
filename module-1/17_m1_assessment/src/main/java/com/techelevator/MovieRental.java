f552eeba8686597697215df16362380d44866664
conner eberhardt <ceberhardt1005@gmail.com>
2024-06-05 09:18:50 -0400

    step 1 and 2 complete

---
+++ module-1/17_m1_assessment/src/main/java/com/techelevator/MovieRental.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+public class MovieRental {
+    private String title;
+
+    private String format;
+
+    private boolean isPremiumMovie;
+
+    private double rentalPrice;
+
+    
+}
