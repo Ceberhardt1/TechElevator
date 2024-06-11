299fbdcfb46f80195a499f13a98772e88474ef81
conner eberhardt <ceberhardt1005@gmail.com>
2024-06-11 08:33:38 -0400

    msg

--- module-2/01_Intro_to_Databases/exercise/Exercises/14_new_states.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/14_new_states.sql
@@ -1,8 +1,7 @@
 -- 14. The state name, nickname, and census region for states that start with the word "New".
 -- Order the results by census region alphabetically, then state nickname alphabetically.
 -- (4 rows)
-
-select state_name, state_nickname, census_region
-From state
-Where state_name like 'New%'
-Order by census_region asc, state_name asc;
+SELECT state_name,state_nickname,census_region
+FROM state
+WHERE state_name LIKE 'New%'
+ORDER BY census_region ASC, state_nickname ASC;
