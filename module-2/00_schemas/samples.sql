05156af356ab9b1001fded043c841b1d03def276
conner eberhardt <ceberhardt1005@gmail.com>
2024-06-12 09:04:43 -0400

    6/12/2024

--- module-2/00_schemas/samples.sql
+++ module-2/00_schemas/samples.sql
@@ -40,8 +40,33 @@ Where state_abbreviation = 'PA'
 Order by city_name desc;
 
 
+
+
+
+SELECT park_name, date_established
+From park
+Where date_established BETWEEN '1960-01-01' and '1969-12-31'
+Order by date_established desc;
+
+Select state_name, state_nickname
+From state
+Order by population desc
+limit 1;
+
 Select *
 From State;
+
+
+
+----Hw 02 subquires and aggregations
+
+--1).
+Select (city_name || ' ,' || state_abbreviation) AS name_and_state, population
+from city
+Where state_abbreviation in ('Pa', 'WV', 'KY', 'IN', 'MI')
+Order By state_abbreviation asc, city_name asc;
+
+
 
 
 
--- module-2/02_Subqueries_and_Aggregation/exercise/Exercises/01_states_border_Ohio.sql
+++ module-2/02_Subqueries_and_Aggregation/exercise/Exercises/01_states_border_Ohio.sql
@@ -3,4 +3,7 @@
 -- The city name and state abbreviation should be returned as a single column called 'name_and_state' and should contain values such as "Detroit, MI".
 -- Order the results alphabetically by state abbreviation and then by city name.
 -- (20 rows)
-
+Select (city_name|| ', ' || state_abbreviation) AS name_and_state, population
+from city
+Where state_abbreviation IN ('PA', 'WV', 'KY', 'IN', 'MI')
+Order By state_abbreviation asc, city_name asc;
