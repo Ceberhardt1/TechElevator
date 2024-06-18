c654838666074e6831b39526d73f9a63591512dd
conner eberhardt <ceberhardt1005@gmail.com>
2024-06-18 16:12:52 -0400

    6/18/2024 push hw mondays

--- module-2/06_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcMovieDao.java
+++ module-2/06_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcMovieDao.java
@@ -1,7 +1,9 @@
 package com.techelevator.movies.dao;
 
+import com.techelevator.movies.model.Genre;
 import com.techelevator.movies.model.Movie;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -17,22 +19,61 @@ public class JdbcMovieDao implements MovieDao {
 
     @Override
     public List<Movie> getMovies() {
-        return null;
+        List<Movie> movies = new ArrayList<>();
+        String sql = "SELECT movie_id, title, overview, tagline, poster_path, home_page, release_date, length_minutes, director_id, collection_id FROM movie";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while(results.next()){
+            movies.add(mapRowToMovie(results));
+        }
+        return movies;
     }
 
     @Override
-    public Movie getMovieById(int id) {
-        return new Movie(-1, "Not implemented yet", "", "", "", "", null, 0, -1, -1);
+    public Movie getMovieById(int movieId) {
+        Movie movies = null;
+        String sql = " SELECT movie_id, title, overview, tagline, poster_path, home_page, release_date, length_minutes, director_id, collection_id " +
+                "FROM movie " +
+                "WHERE movie_id = ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, movieId);
+        if(results.next()){
+            movies = mapRowToMovie(results);
+        }
+        return movies;
     }
 
     @Override
     public List<Movie> getMoviesByTitle(String title, boolean useWildCard) {
-        return null;
+        List<Movie> movies = new ArrayList<>();
+
+        String sql = "SELECT movie_id, title, overview, tagline, poster_path, home_page, release_date, length_minutes, director_id, collection_id \n" +
+                "FROM movie " +
+                "WHERE title ILIKE ?";
+        if(useWildCard){
+            title = "%" + title + "%";
+        }
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, title);
+        while(results.next()){
+            movies.add(mapRowToMovie(results));
+        }
+        return movies;
     }
 
     @Override
     public List<Movie> getMoviesByDirectorNameAndBetweenYears(String directorName, int startYear,
                                                               int endYear, boolean useWildCard) {
         return null;
+    }
+    private Movie mapRowToMovie(SqlRowSet rowSet){
+        Movie movies= new Movie();
+        movies.setId(rowSet.getInt("movie_id"));
+        movies.setTitle(rowSet.getString("title"));
+        movies.setOverview(rowSet.getString("overview"));
+        movies.setTagline(rowSet.getString("tagline"));
+        movies.setPosterPath(rowSet.getString("poster_path"));
+        movies.setReleaseDate(rowSet.getDate("release_date").toLocalDate());
+        movies.setLengthMinutes(rowSet.getInt("length_minutes"));
+        movies.setDirectorId(rowSet.getInt("director_id"));
+        movies.setCollectionId(rowSet.getInt("collection_id"));
+        return movies;
     }
 }
--- module-2/06_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcPersonDao.java
+++ module-2/06_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcPersonDao.java
@@ -1,7 +1,9 @@
 package com.techelevator.movies.dao;
 
+import com.techelevator.movies.model.Movie;
 import com.techelevator.movies.model.Person;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -17,21 +19,62 @@ public class JdbcPersonDao implements PersonDao {
 
     @Override
     public List<Person> getPersons() {
-        return null;
+        List<Person> person = new ArrayList<>();
+        String sql = "SELECT person_id, person_name, birthday, deathday, biography, profile_path, home_page FROM person ";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while(results.next()){
+            person.add(mapRowToPerson(results));
+        }
+        return person;
     }
 
     @Override
-    public Person getPersonById(int id) {
-        return new Person(-1, "Not implemented yet", null, null, "", "", "");
+    public Person getPersonById(int personId) {
+        Person person = null;
+        String sql = " SELECT person_id, person_name, birthday, deathday, biography, profile_path, home_page " +
+                "FROM person " +
+                "WHERE person_id = ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, personId);
+        if(results.next()){
+            person = mapRowToPerson(results);
+        }
+        return person;
     }
 
     @Override
     public List<Person> getPersonsByName(String name, boolean useWildCard) {
-        return null;
+        List<Person> person = new ArrayList<>();
+
+        String sql = "SELECT person_id, person_name, birthday, deathday, biography, profile_path, home_page \n" +
+                "FROM person " +
+                "WHERE person_name ILIKE ?";
+        if(useWildCard){
+            name = "%" + name + "%";
+        }
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, name);
+        while(results.next()){
+            person.add(mapRowToPerson(results));
+        }
+        return person;
     }
 
     @Override
     public List<Person> getPersonsByCollectionName(String collectionName, boolean useWildCard) {
         return null;
+    }
+    private Person mapRowToPerson(SqlRowSet rowSet){
+        Person person = new Person();
+        person.setId(rowSet.getInt("person_id"));
+        person.setName(rowSet.getString("person_name"));
+        if(rowSet.getDate("birthday") != null){
+            person.setBirthday(rowSet.getDate("birthday").toLocalDate());
+        }
+        if(rowSet.getString("biography") != null){
+            person.setDeathDate(rowSet.getDate("deathday").toLocalDate());
+        }
+        person.setBiography(rowSet.getString("biography"));
+        person.setProfilePath(rowSet.getString("profile_path"));
+        person.setHomePage(rowSet.getString("home_page"));
+        return person;
     }
 }
