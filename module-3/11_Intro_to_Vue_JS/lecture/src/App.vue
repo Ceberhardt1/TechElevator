d923c0c9bee89c2a75c0f2d77dfe7d9d477b190d
conner eberhardt <ceberhardt1005@gmail.com>
2024-07-22 09:21:26 -0400

    pulling

--- module-3/11_Intro_to_Vue_JS/lecture/src/App.vue
+++ module-3/11_Intro_to_Vue_JS/lecture/src/App.vue
@@ -2,15 +2,20 @@
   <div class="main">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <br/>
+    <ProductReview/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ProductReview from './components/ProductReview.vue';
+import HelloWorld from './components/HelloWorld.vue'; 
+
 
 export default {
   components: {
-    HelloWorld
+    HelloWorld, 
+    ProductReview
   }
 }
 </script>
@@ -21,7 +26,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f1eaea;
   margin-top: 60px;
 }
+body{
+  background-image: url(./assets/vue-lecture-introbackground.webp);
+  background-size: cover;
+  
+  
+}
+&:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient( rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.34)); /* Adjust the rgba values and gradient direction */
+  }
 </style>
---
+++ module-3/11_Intro_to_Vue_JS/lecture/src/components/ProductReview.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="product-review">
+        <h3>{{ name }}</h3>
+        <p>{{ description }}</p>
+    </div>
+    
+</template>
+  
+  <script>
+  export default {
+        data(){
+            return{
+                name: 'Head First Design',
+                description: "This is the description"
+            }
+        }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  .product-review{
+    
+  }
+
+  
+  </style>
+  
