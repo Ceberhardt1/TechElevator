ec02b6ffe689cb42b04672ff94553d4345de3e01
Steve Carmichael <steve@techelevator.com>
2024-07-31 19:25:45 -0400

    added final lecture code

---
+++ module-3/19_Final_Review_Day/project-review-day1/vue/src/components/NasaPictureOfDay.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Nasa Picture of the Day</h2>
+    <button v-on:click="getInfo" type="button" class="btn btn-outline-primary">Click me to win a prize!!!</button>
+
+    <div v-show="info != {}">
+       <img v-bind:src="info.url" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+
+import NasaService from '../services/NasaService.js';
+
+export default {
+
+    data() {
+        return {
+            info: {}
+        }
+    },
+    methods: {
+       getInfo() {
+           console.log('I am here');
+           NasaService.getNASAData().then( response => {
+            this.info = response.data;
+           })
+       }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
